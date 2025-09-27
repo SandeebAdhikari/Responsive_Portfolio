cad02f8294651c5338043816ac9016214efbc5f1
SandeebAdhikari <sa2734@njit.edu>
2025-09-27 14:17:49 -0400

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 export const dynamic = "force-dynamic";
 
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -18,6 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G1PXHE3QBS"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-G1PXHE3QBS');
+    `}
+      </Script>
+
       <body className="courier-font">{children}</body>
     </html>
   );
